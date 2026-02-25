43347ae5dfd148cb3f0d99835f0ff2f912b07d46
Anil_sain_1729 <asain4773@gmail.com>
2026-02-25 12:03:25 +0530

    Add files via upload

---
+++ power.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int power(int a , int b){
+    if(b==0) return 1;
+    return a*power(a,b-1);
+}
+int main(){
+    int a,b;
+    printf("enter the valu : ");
+    scanf("%d %d",&a,&b);
+    int p=power(a,b);
+    printf(" %d",p);
+    return 0;
+}
