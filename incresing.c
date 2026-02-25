52d1ddd8b8a39bc66131f49ef51a0dde2eb1e687
Anil_sain_1729 <asain4773@gmail.com>
2026-02-25 12:06:17 +0530

    Add files via upload

---
+++ incresing.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void in(int x, int n){
+    if(x>n) return;
+    printf("%d\n",x);
+    in(x+1,n);
+    return ;
+}
+int main(){
+    int n;
+    printf("enter the nummber ");
+    scanf("%d",&n);
+    in(1,n);
+    return 0;
+}
