c58a2802ac9761959a9d29547959368bba633ef6
Anil_sain_1729 <asain4773@gmail.com>
2026-02-25 11:57:17 +0530

    Add files via upload

---
+++ transposeMatrix.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+   int n;
+   printf("enter the n  ");
+   scanf("%d",&n);
+   int a[n][n];
+   //input
+   for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+           scanf("%d",&a[i][j]);
+       }
+   }
+   //transpose
+   for(int i=0;i<n;i++){
+      for(int j=0;j<i;j++){
+         int temp = a[i][j];
+         a[i][j] = a[j][i];
+         a[j][i] = temp;
+      }
+   }
+   // output
+   for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+           printf("%2d",a[i][j]);
+       }
+       printf("\n");
+   }
+   return 0;
+}
