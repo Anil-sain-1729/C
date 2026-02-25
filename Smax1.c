126688ea0d2a236b0554b641f63c390c84017e12
Anil_sain_1729 <asain4773@gmail.com>
2026-02-25 12:00:11 +0530

    Add files via upload

---
+++ Smax1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<limits.h>
+int main(){
+    int arr[7]={1,5,2,4,7,2,6};
+    int max= INT_MIN;
+    int smax= INT_MIN;
+    for(int i=1;i<7;i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    for(int i=1;i<7;i++){
+        if(arr[i]!=max && smax<arr[i]){
+           smax=arr[i];
+        }
+    }
+    printf("%d",smax);
+}
